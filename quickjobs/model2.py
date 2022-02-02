32d3773be65c542e3c54a0a6c5352da03fa68322
reesh19 <rodriguezr235@gmail.com>
2022-02-01 18:01:44 -0800

    v1.1

--- quickjobs/model2.py
+++ quickjobs/model2.py
@@ -1,17 +1,26 @@
 from googlesearch import search
 from datetime import datetime as dt
 from datetime import timedelta as td
-from pytz import timezone as tz
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
+import requests
 
 titles = ["Data Scientist",
           "Machine Learning Engineer",
           "ML Engineer",
           "Machine Learning",
-          "Data Science"]
+          "Data Science",
+          "Data Analyst",
+          "Quantitative Analyst",
+          "Python",
+          "Pandas",
+          "Scikit-Learn",
+          "TensorFlow",
+          "Keras",
+          "Pytorch",
+          "Artificial Intelligence",
+          "Deep Learning"]
 
-def get_jobs(loc="los angeles", remote=False, job_titles=["Data Scientist", "Machine Learning Engineer", "ML Engineer", "Machine Learning", "Data Science"], ignore_director=True, max_results=20, n_days=3):
+def get_jobs(loc="Los Angeles", remote=False, job_titles=titles, ignore_director=True, max_results=20, n_days=7):
     """
     Performs hyper-precise job searches on 4 popular job boards
     -----------------------------------------------------------
@@ -22,18 +31,18 @@ def get_jobs(loc="los angeles", remote=False, job_titles=["Data Scientist", "Mac
 
             Jobs in Los Angeles:
                 > get_jobs(loc='los angeles')
-            
+
             Jobs in France:
                 > get_jobs(loc='france')
-    
+
 
     remote (bool):
-        While True, only remote jobs will be considered, however it is recommended 
-        to set 'loc' to your home country to avoid non-relevant results.
+        While True, only remote jobs will be considered. 
+        Default value is False.
 
             ONLY remote jobs:
                 > get_jobs(remote=True)
-            
+
             All jobs:
                 > get_jobs(remote=False)
 
@@ -44,14 +53,14 @@ def get_jobs(loc="los angeles", remote=False, job_titles=["Data Scientist", "Mac
 
             Single title:
                 > get_jobe(job_titles='Propulsion Engineer')
-            
+
             Multiple titles:
                 > get_jobs(job_titles=['Data Engineer','Cook','Librarian'])
-    
-    
+
+
     ignore_director (bool):
         If True, job titles which include the word 'Director' will be ignored
-    
+
 
     max_results (int):
         Maximum number of results (per job board)
@@ -81,75 +90,93 @@ def get_jobs(loc="los angeles", remote=False, job_titles=["Data Scientist", "Mac
         > get_jobs('alaska', True, 'fireman', 10, 2)
 
     """
-    urls = ['jobs.lever.co', 
-            'apply.workable.com', 
-            'boards.greenhouse.io', 
+    urls = ['jobs.lever.co',
+            'apply.workable.com',
+            'boards.greenhouse.io/*/jobs',
             'jobs.jobvite.com/*/job']
-    
+
     s = ''
 
-    if type(loc) == str:
-        s += f""""{loc}" """
-    
-    elif type(loc) == list and all(type(i) == str for i in loc):
-        l = 0
-        
-        for i in loc:
-            s += f""""{i}" """
-            l += 1
-            
-            if l < len(loc):
-                s += 'OR '
-    
-    else: pass
+    if loc:
 
-    if type(job_titles) == str:
-        s += f""""{job_titles}" """
-    
-    elif type(job_titles) == list and all(type(i) == str for i in job_titles):
-        n = 0
+        if type(loc) == str:
+            s += f""""{loc}" """
 
-        for i in job_titles:
-            s += f""""{i}" """
-            n += 1
-            
-            if n < len(job_titles):
-                s += 'OR '
-    
-    else: pass
-    
+        elif type(loc) == list and all(type(i) == str for i in loc):
+            l = 0
+
+            for i in loc:
+                s += f""""{i}" """
+                l += 1
+
+                if l < len(loc):
+                    s += 'OR '
+
+        else: pass
+
+    if job_titles:
+
+        if type(job_titles) == str:
+            s += f""""{job_titles}" """
+
+        elif type(job_titles) == list and all(type(i) == str for i in job_titles):
+            n = 0
+
+            for i in job_titles:
+                s += f""""{i}" """
+                n += 1
+
+                if n < len(job_titles):
+                    s += 'OR '
+
+        else: pass
+
     if remote:
-        s += """"remote" """
-    
+        s += 'remote '
+
     if ignore_director: 
-            s += '-director'
+            s += '-Director -Principal -Lead'
 
-    dtformat = "%Y-%M-%D"
-    yesterday = dt.now().astimezone(tz('US/Pacific')) - td(n_days)
-    t_str = yesterday.astimezone(tz('US/Pacific')).strftime(dtformat)
+    dtformat = "%Y-%m-%d"
+    t0 = dt.now() - td(n_days)
+    t_str = t0.strftime(dtformat)
 
-    queries = [f""""{s} site:https://{url}/* after:{t_str}""" for url in urls]
+    queries = [f"""{s} site:https://{url}/* after:{t_str}""" for url in urls]
+
+    agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15'
 
     results = [] 
-    # results = [[list0],[list1], ..] 
-    # results[0] = [job0, job1, ...]
-    # results[0][0] = (job title, job url)
-
     count = 0
 
     for i in queries:
+        # import requests
+        # from requests_ip_rotator import ApiGateway, EXTRA_REGIONS
+
+        # gateway = ApiGateway("https://www.transfermarkt.es")
+        # gateway.start()
+
+        # session = requests.Session()
+        # session.mount("https://www.transfermarkt.es", gateway)
+
+        # response = session.get("https://www.transfermarkt.es/jadon-sancho/profil/spieler/your_id")
+        # print(response.status_code)
+
+        # # Only run this line if you are no longer going to run the script, as it takes longer to boot up again next time.
+        # gateway.shutdown() 
         res = []
-        
-        for url in i:
-            
-            for reply in search(url, stop=max_results, pause=3):
-                soup = bs(urlopen(reply))
-                
-                res.append((soup.title.get_text(), reply))
-                count += 1
-        
+        session = requests.Session()
+        session.headers['User-Agent'] = agent
+
+        for url in search(i, stop=max_results, pause=4, user_agent=agent):
+            req = session.get(url)
+            soup = bs(req.content,) #, 'html.parser')
+            title = soup.title.get_text()
+
+            res.append((title, url))
+            count += 1
+
         results.append(res)
 
     message = f'Found {count} jobs posted in the last {n_days} day(s):'
-    
+
     return message, results
